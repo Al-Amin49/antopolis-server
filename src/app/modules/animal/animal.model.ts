6a8c55e93869914a8a0f9c8ee535625b7d64b33d
Al-Amin49 <alamin496832@gmail.com>
2024-11-28 22:38:04 +0600

    update: animal model is updated

--- src/app/modules/animal/animal.model.ts
+++ src/app/modules/animal/animal.model.ts
@@ -9,6 +9,11 @@ const animalSchema= new mongoose.Schema(
         image:{
             type:String,
             required:[true, 'Image is required']
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Category',
+            required:[true, 'Category is required']
         }
     },
     {timestamps:true}
